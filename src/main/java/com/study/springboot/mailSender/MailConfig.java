a5079c958fa19db8ab75991db31be66bbd3cb322
HedeunKim\user <jcdeun91@gmail.com>
2024-10-07 01:46:28 +0900

    아이디/비밀번호 찾기 작업 완료
    - build.gradle : dotenv 의존성 추가
    - dotenv 를 사용하여 .env 파일에서 설정값을 불러오는 방식으로 수정
    - find_info : 이메일 마스킹 함수 수정

--- src/main/java/com/study/springboot/mailSender/MailConfig.java
+++ src/main/java/com/study/springboot/mailSender/MailConfig.java
@@ -1,30 +1,27 @@
 package com.study.springboot.mailSender;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import java.util.Properties;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @Configuration
 public class MailConfig {
 
-    @Value("${spring.mail.username}")
-    private String username; // Gmail 계정 사용자 이름
-
-    @Value("${spring.mail.password}")
-    private String password;
+	private final Dotenv dotenv = Dotenv.load(); 
 
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com"); // Gmail의 SMTP 서버 주소
-        mailSender.setPort(587); // Gmail SMTP 포트
+        mailSender.setHost(dotenv.get("EMAIL_HOST")); // Gmail의 SMTP 서버 주소
+        mailSender.setPort(Integer.parseInt(dotenv.get("EMAIL_PORT"))); // SMTP 포트
 
-        mailSender.setUsername(username); // 사용자 이름 설정
-        mailSender.setPassword(password); // 환경 변수에서 가져온 비밀번호 설정
+        mailSender.setUsername(dotenv.get("EMAIL_USERNAME")); // 사용자 이메일
+        mailSender.setPassword(dotenv.get("EMAIL_PASSWORD")); // 이메일 비밀번호
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
@@ -34,7 +31,6 @@ public class MailConfig {
         
         mailSender.setJavaMailProperties(props);
         mailSender.setDefaultEncoding("UTF-8");
-출처: https://cn-c.tistory.com/95 [Codename Cathy:티스토리]
 
         return mailSender;
     }
