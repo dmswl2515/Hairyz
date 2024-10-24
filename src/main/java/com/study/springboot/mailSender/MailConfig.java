50df4a43f803cc263b81b8a7f06788f4c59a8630
DESKTOP-25TV5SJ\BigData <jcdeun91@gmail.com>
2024-10-24 10:40:43 +0900

    JavaMailSender 관련 설정 변경 - API KEY 저장위치 변경 - dotenv 의존성 삭제

--- src/main/java/com/study/springboot/mailSender/MailConfig.java
+++ src/main/java/com/study/springboot/mailSender/MailConfig.java
@@ -2,26 +2,35 @@ package com.study.springboot.mailSender;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @Configuration
 public class MailConfig {
+	
+	@Value("${EMAIL_HOST}")
+    private String EMAIL_HOST;
 
-	private final Dotenv dotenv = Dotenv.load(); 
+    @Value("${EMAIL_PORT}")
+    private int EMAIL_PORT;
+
+    @Value("${EMAIL_USERNAME}")
+    private String EMAIL_USERNAME;
+
+    @Value("${EMAIL_PASSWORD}")
+    private String EMAIL_PASSWORD;
 
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(dotenv.get("EMAIL_HOST")); // Gmail의 SMTP 서버 주소
-        mailSender.setPort(Integer.parseInt(dotenv.get("EMAIL_PORT"))); // SMTP 포트
+        mailSender.setHost(EMAIL_HOST); // Gmail의 SMTP 서버 주소
+        mailSender.setPort(EMAIL_PORT); // SMTP 포트
 
-        mailSender.setUsername(dotenv.get("EMAIL_USERNAME")); // 사용자 이메일
-        mailSender.setPassword(dotenv.get("EMAIL_PASSWORD")); // 이메일 비밀번호
+        mailSender.setUsername(EMAIL_USERNAME); // 사용자 이메일
+        mailSender.setPassword(EMAIL_PASSWORD); // 이메일 비밀번호
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
